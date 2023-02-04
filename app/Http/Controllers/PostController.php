47d140a5983f7e26c751d4425ce375e1f05a6c76
Dave <87390532+vadedes@users.noreply.github.com>
2023-02-04 22:25:23 +0800

    implemented edit post function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,28 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
+    public function updatePost(Post $post, Request $request) {
+        //validate request
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        //strip down incoming tags
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        //actually update database
+        $post->update($incomingFields);
+
+        //redirect user back to the previous url
+        return back()->with('success', 'Post successfully updated.');
+    }
+
+    public function showEditForm(Post $post) {
+        return view('edit-post', ['post' => $post]);
+    }
+
     public function delete(Post $post) {
         $post->delete();
         return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::get('/create-post', [PostController::class, 'showCreateForm'])->middlewar
 Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('MustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->middleware('MustBeLoggedIn');
 Route::delete('/post/{post}', [PostController::class, 'delete'])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, 'updatePost'])->middleware('can:update,post');
 
 //Profile related routes
 Route::get('/profile/{user:username}', [UserController::class, 'profile']);
