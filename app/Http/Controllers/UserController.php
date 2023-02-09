b640194923699cbcb4d20ec8d040c1117f3f15f5
Dave <dave@vadedesign.com>
2023-02-09 15:02:19 +0800

    Added database seeder code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -148,7 +148,7 @@ class UserController extends Controller
 
     public function showCorrectHomepage() {
         if (auth()->check()) {
-            return view('homepage-feed', ['posts' => auth()->user()->feedPosts()->latest()->paginate(4)]);
+            return view('homepage-feed', ['posts' => auth()->user()->feedPosts()->latest()->paginate(6)]);
         } else {
             return view('homepage');
         }
